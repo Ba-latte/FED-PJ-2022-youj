f8d75cd4442b65b168553e8f3daf6013de08b0b3
Ba-latte <youj_lim@naver.com>
2023-02-23 16:23:39 +0900

    2/23 오후 수업

--- 700.쉐도잉프로젝트/02.VOGUE_PJ/002.site/js/main.js
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/002.site/js/main.js
@@ -45,21 +45,70 @@ window.addEventListener("DOMContentLoaded", ()=>{
     -등장액션 대상 위치값 리턴 함수 만들기
     -스크롤 이벤트 세팅하기
         -값 확인해보기
+    -클래스 넣기 함수 만들기
     ***********************************************************************/
-    // ⭐1.대상선정 : .scAct
+    // ⭐1.대상선정
+    // 스크롤 등장 대상 - .scAct
     const scAct = q(".scAct");
+    // 상단메뉴 대상 - #top
+    const topA = q("#top");
+    // 상단이동버튼 대상 - .tbtn
+    const tbtn = q(".tbtn");
+    // 화면높이값의 2/3구하기
+    const hv = window.innerHeight/3*2;
+    // console.log("2/3높이:",hv);
+    // cg(tbtn);
 
-    // ⭐스크롤 이벤트 세팅하기
+
+
+
+
+    // ⭐2.클래스 넣기 함수 만들기 ///////
+    const showIt = ele => { // ele - 등장요소
+        // 대상요소의 현재스크롤 위치
+        let xval = retVal(ele);
+
+        // 구간적용여부 검사하기
+        // 0보다 크고 화면의 2/3보다 작은 구간!
+        if(xval < hv && xval > 0){
+            // console.log("작동!~~~~");
+            // 해당요소에 클래스 넣기!
+            ele.classList.add("on");
+        }
+        // 만약 되돌리고 싶다면! else문으로 on을 제거하게 만들면 된다
+        // else{
+        //     ele.classList.remove("on");
+        // }
+    }; //////////// showIt //////////
+
+
+
+
+    // ⭐4.현재 스크롤위치 담을 변수 만들기
+    let scTop;
+
+
+    // ⭐3.스크롤 이벤트 세팅하기
     window.addEventListener("scroll", ()=>{
-
         // 값 확인하기
-        cg("박스1: "+retVal(scAct[0]));
+        // cg("박스1: "+retVal(scAct[0]));
+
+        // 현재 스크롤 위치 확인하기
+        scTop = window.scrollY;
+        // cg(stop);
+        
+        // ⭐5.상단영역 슬림메뉴 적용하기 (만약 scTop가 100보다 같거나 크다면, on클래스 추가하고, 그외에는 on클래스 제거해라)
+        if(scTop >= 100) topA.classList.add("on");
+        else topA.classList.remove("on");
+
+        // ⭐6.상단 이동버튼 보이기/숨기기
+        if(scTop >= 300) tbtn.classList.add("on");
+        else tbtn.classList.remove("on");
+
+        // 스크롤등장 요소 개수만큼 for문으로 돌리기
+        for(let x of scAct) showIt(x);
 
     }); ////////////////////////////// scroll 이벤트 끝 ///////////////////////////////////
-
-
-
-
 
 
 
