43e54380fc5d0a1cca860e3c0f537372d94aa591
Ba-latte <youj_lim@naver.com>
2023-04-26 13:08:55 +0900

    4/26 오전 수업

--- 005.jQuery학습/002.실전연습/03_Click_PhotoViewer/js/multi.js
+++ 005.jQuery학습/002.실전연습/03_Click_PhotoViewer/js/multi.js
@@ -1,7 +1,18 @@
 // 무한이동 드래그&클릭형&멀티 배너 JS - multi.js
 
 
-// 제이쿼리 코드 블록 /////////////////////
-$(() => {
-    
-}); //////////////// jQB ////////////////////
+// 공통 슬라이드 함수 import하기
+import mySlider from "./mySlider.js";
+
+// 함수 호출하기
+// 현재 슬라이드 3군데 모두 적용하기
+// 제이쿼리버전
+$(".slider").each((idx, ele)=>{
+    // console.log(ele);
+    mySlider(ele);
+});
+
+// js버전
+// document.querySelectorAll(".slider").forEach((ele)=>{
+//     mySlider(ele);
+// });
