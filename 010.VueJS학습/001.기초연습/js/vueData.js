3018f704c567e8d6bec92ad07c0007183a773f31
Ba-latte <youj_lim@naver.com>
2023-05-12 13:56:00 +0900

    5/12 오전 수업

--- 010.VueJS학습/001.기초연습/js/vueData.js
+++ 010.VueJS학습/001.기초연습/js/vueData.js
@@ -4,14 +4,21 @@
 import store from "./vuedata-store.js";
 
 
-// 컴포넌트 생성하기
+// [ 컴포넌트 생성하기 ]
+// -이미지 src속성 바인드시 (v.idx>50?1:v.idx) 문장 해석
+// : 데이터 idx값이 50 초과이면 1을 넣고 아니면 idx데이터 넣기
+// -엑시오스로 가져온 데이터는 {data:{실제 데이터}} 형식으로 한번 더 감싸서 들어옴
+// -따라서 실제 데이터를 사용하려면 "$store.state.items.data" 이런식으로 "data"속성까지 써줘야 바로 사용 가능함
+
+// 반면 뷰엑스 스토어의 actions에서 받은 제이슨 데이터는 원본 그대로 할당하여 들어오므로 "$store.state.items"라고 원본 변수를 바로 사용해야 함!
+
 Vue.component("my-comp",{
     // 뷰.컴포넌트(들어갈모체선택(사용자지정태그명), {옵션})
     template:`
     <div class="grid">
-        <div v-for="(v,i) in $store.state.items.data">
+        <div v-for="(v,i) in $store.state.items">
             <img v-bind:src="
-                './img_gallery/' + v.idx + '.jpg'
+                './img_gallery/' + (v.idx>50?1:v.idx) + '.jpg'
             " alt="dress">
             <aside>
                 <h2>{{v.gname}}</h2>
@@ -40,13 +47,21 @@ new Vue({
     },
     // 👇뷰 인스턴스 생성 직후(가상DOM/DOM 생성 전) 구역 - 데이터는 미리 읽어서 깔아둘 수 있으니까 여기에!
     created(){
+
+        // 뷰엑스 스토어 액션스 구역 메서드 initData()호출하기
+        store.dispatch("initData");
+
+
         // 엑시오스 사용하여 제이슨 데이터 가져오기
         // axios.get(제이슨파일).then(데이터=>담을변수=데이터);
         // 제이슨파일이 받아지고나면(then그러고나면) 할당해준다는 뜻임
-        axios.get("./js/data.json")
-        .then(x=>store.state.items = x);
+        // axios.get("./js/goods.json")
+        // .then(x=>store.state.items = x);
 
         // 스토어의 items 전역변수에 세팅함!
-        console.log(store.state.items);
+        // console.log(store.state.items);
+
+
+        
     }, ////////////// created ////////////////
 }); //////////////// 뷰 인스턴스 /////////////////////
--- 010.VueJS학습/001.기초연습/js/vuedata-store.js
+++ 010.VueJS학습/001.기초연습/js/vuedata-store.js
@@ -1,5 +1,23 @@
 // 뷰JS 데이터처리 뷰엑스 스토어 JS - vuedata-store.js
 
+// 제이슨 데이터 처리 위해 제이슨 불러오기
+/**************************************************************************************
+
+import jsnData from "./goods.json";
+-일반 JS호출과 같은 방식으로 제이슨 파일을 호출하면 아래와 같은 MIME 전송형식 type 에러가 발생함!!
+"Failed to load module script: Expected a JavaScript module script but the server responded with a MIME type of "application/json". Strict MIME type checking is enforced for module scripts per HTML spec."
+
+-해결방법 : 아래처럼 assert{} 객체를 추가로 적어주기
+*assert : 주장하다, 강하게 자기 주장을 하다, 자신의 권리,권위 등을 확고히 하다
+-import 변수 from "제이슨파일경로" assert{type:"json"};
+-assert{type:"json"} : import시 형식을 지정해주는 객체임
+-이것으로 제이슨 형식을 맞춰서 전송타입 오류를 없애야함
+
+**************************************************************************************/
+import jsnData from "./goods.json" assert{type:"json"};
+
+
+
 ////////////////////////////// 😊뷰엑스 스토어를 활용한 변수 세팅하기😊 /////////////////////////////////////
 // 1.뷰엑스 스토어 인스턴스를 생성한다
 const store = new Vuex.Store({
@@ -12,12 +30,35 @@ const store = new Vuex.Store({
 
     // (2)데이터 변경 메서드 구역 : 호출시 commit() 사용!
     mutations:{
+        // 제이슨 데이터 속성 변수 업데이트하기 함수명(스테이트, 파라미터)
+        setData(st, pm){
+            // st-state변수(데이터 세팅구역?), pm-전달변수
+            // 액션스에서 커밋으로 뮤테이션스 안의 이 함수를 불러들일 예정임
 
+            st.items = pm;
+            // 👆state구역의 items변수에 제이슨 데이터 담기
+
+            console.log("뮤테이션스: ", pm);
+            // pm값에 어떤게 넘어와서 할당이 되는지 확인해봄
+        },
     },
-    
-    // (3)백엔드 관련 코딩 비동기 처리 메서드 구역 : 호출시 dispatch() 사용!
-    actions:{
 
+    // (3)백엔드 관련 코딩 비동기 처리 메서드 구역 : 호출시 dispatch() 사용!
+    // -얘는 얘대로 데이터 처리해서 가져온대
+    // -여기서 불러와서 처리한 데이터를.... created구역에 넣어주면.... 돔만들어지기 전에 처리해서 데이터를 넣는거니까..데이터가 없다고 표시되지 않는대!
+    actions:{
+        // 제이슨 데이터 로드하기 메서드
+        // 👇액션스 메서드 전달값으로 {commit}을 쓰면 뮤테이션 구역으로 바로 commit 사용 가능함 (중괄호 구역에서 바로 커밋사용 가능하단 의미임)
+        initData({commit}){
+            // 제이슨 데이터를 변수에 할당하기
+            const result = jsnData;
+            console.log("액션구역!: ", result);
+            // ->이렇게 하면 객체 자체가 다 들어옴
+
+            // state의 items 변수 변경하는 메서드를 호출하기
+            // 뮤테이션스의 메서드 호출은? commit('메서드명', 전달할 값)으로 하기
+            commit("setData", result);
+        }, /////////////// initData /////////////////
     },
     
 }); /////////////////////////// 뷰엑스 스토어 인스턴스 /////////////////////////////////////
