11eb48d2271b48d0b415f950739412f3bf8a4b43
Ba-latte <youj_lim@naver.com>
2023-05-31 16:18:07 +0900

    5/31 오후 수업

---
+++ 014.React학습/react-app/youj-app/src/Test.js
@@ -0,0 +1,16 @@
+// 테스트 모듈 JS
+
+// 이 모듈에서만 적용할 CSS 불러오기
+import "./test.css";
+
+function Test(){
+  return(
+    <>
+      <h1 className="tit">나야나😎</h1>
+      <img src='https://dimg.donga.com/wps/NEWS/IMAGE/2015/12/11/75316598.3.jpg' alt='모네 그림' />
+    </>
+  );
+}
+
+// 내보내기
+export default Test;
