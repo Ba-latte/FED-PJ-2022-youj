c042df9c7299af08e66bb9dc5544791dcfe0d2c3
Bara <youj_lim@naver.com>
2023-05-31 19:35:44 +0900

    5/31

---
+++ 014.React학습/react-app/youj-app2/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Test from './Test';
+
+
+
+
+// 랜더링하기
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Test />);
