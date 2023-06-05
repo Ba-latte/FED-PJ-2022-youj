3d1484b1b7ec0e925eea464a1173e98e3dacb500
Ba-latte <youj_lim@naver.com>
2023-06-05 16:22:31 +0900

    6/5 오후 수업

--- 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/MenuBtn.js
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/MenuBtn.js
@@ -3,6 +3,7 @@
 // 여기서 쓸 것들만 import 하기
 import $ from 'jquery';
 import './css/menubtn.css';
+import menubtn_data from './data/menubtn';
 
 // 제이쿼리 로드구역 함수
 function jqFn(){
@@ -12,24 +13,37 @@ function jqFn(){
 } ///////////////////// jqFn 함수 /////////////////////
 
 
+function MakeSlide(props){
+    return(
+        <>
+            <div>
+                <div className="imbx">
+                    <img src={props.rec["isrc"]} alt="배너이미지" />
+                </div>
+                <div className="titbx">
+                    <h3>{props.rec["tit"].split("^", 1)}</h3>
+                    <h2>{props.rec["tit"].split("^", 2)}</h2>
+                </div>
+                <div className="btnbx">
+                    <button>{props.rec["btn"]}</button>
+                </div>
+            </div>
+        </>
+    );
+}
+
+
 
 // 컴포넌트
-function MenuBtn(){
+function MenuBtn(props){
+
+    // 데이터
+    const sel_data = menubtn_data;
+
     return(
         <>
             <section className="menubtn">
-                <div>
-                    <div className="imbx">
-                        <img src="./images/menubtn1.jpg" alt="배너이미지" />
-                    </div>
-                    <div className="titbx">
-                        <h3>WORLDS COLLIDE</h3>
-                        <h2>THE FLASH IS ONLY IN THEATERS JUNE 16</h2>
-                    </div>
-                    <div className="btnbx">
-                        <button>TICKETS ON SALE NOW</button>
-                    </div>
-                </div>
+                {sel_data.map((x, i)=><MakeSlide rec={x} idx={i} key={i} />)}
             </section>
             {/* 바깥에 빈 루트를 만들고 JS 로드 함수 포함시키기 */}
             {jqFn()}
---
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/data/menubtn.js
@@ -0,0 +1,30 @@
+// 메인페이지 MenuBtn 컴포넌트용 데이터 - menubtn.js
+
+const menubtn_data = [
+    {
+        "isrc": "./images/menubtn1.jpg",
+        "tit": "WORLDS COLLIDE^THE FLASH IS ONLY IN THEATERS JUNE 16",
+        "btn": "TICKETS ON SALE NOW",
+        "link": "/co"
+    },
+    {
+        "isrc": "./images/menubtn2.jpg",
+        "tit": "DC SHOP^GET NUTS WITH THE 1/6 SCALE BATMAN FIGURE",
+        "btn": "SHOP NEW ARRIVALS",
+        "link": "/co"
+    },
+    {
+        "isrc": "./images/menubtn3.jpg",
+        "tit": "DC PRIDE^CELEBRATE WITH LGBTQ CREATORS, CHARACTERS, &...",
+        "btn": "VISIT OUR PRIDE HUB",
+        "link": "/gm"
+    },
+    {
+        "isrc": "./images/menubtn4.jpg",
+        "tit": "DC COMMUNITY^SHARE YOUR DC PRIDE FANWORKS",
+        "btn": "SUBMIT YOUR ART",
+        "link": "/nw"
+    }
+];
+
+export default menubtn_data;
